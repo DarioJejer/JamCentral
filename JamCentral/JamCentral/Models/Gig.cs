d3b9c2d762fa90d301a0444c6ef645f9b1c10bae
Dario Jejer <dariojejer@gmail.com>
2020-11-24 15:00:19 -0300

    Create Gig Model

---
+++ JamCentral/JamCentral/Models/Gig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JamCentral.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+        [Required]
+        public ApplicationUser Artist { get; set; }
+        public DateTime Date { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Location { get; set; }
+    }
+}

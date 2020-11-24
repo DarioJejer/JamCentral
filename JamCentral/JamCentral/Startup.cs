91ea43ac92fb240edd7658e1afffd9f4718fd941
Dario Jejer <dariojejer@gmail.com>
2020-11-24 08:54:13 -0300

    Base MVC files

---
+++ JamCentral/JamCentral/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JamCentral.Startup))]
+namespace JamCentral
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

34f546f1249263321938063dd6b2039b4a9b89b8
Dario Jejer <dariojejer@gmail.com>
2020-11-25 10:21:57 -0300

    Created 'Add a Gig' page Front End

---
+++ JamCentral/JamCentral/Controllers/GigsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JamCentral.Models;
+using JamCentral.ViewModels;
+
+namespace JamCentral.Controllers
+{
+    public class GigsController : Controller
+    {
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = new List<Genre>
+                {
+                    new Genre { Id = 1, Name = "Jazz"},
+                    new Genre { Id = 2, Name = "Rock"},
+                    new Genre { Id = 3, Name = "Blues"}
+                }
+            };
+
+            return View(viewModel);
+        }
+    }
+}

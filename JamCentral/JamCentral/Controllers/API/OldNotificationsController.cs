be2f1604056af6f872560bbfad8207b0eed9828b
Dario Jejer <dariojejer@gmail.com>
2020-12-05 14:28:34 -0300

    Add Old Notifications funtionality

---
+++ JamCentral/JamCentral/Controllers/API/OldNotificationsController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using JamCentral.Dtos;
+using JamCentral.Models;
+using JamCentral.Models.NotificationFeed;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace JamCentral.Controllers.API
+{
+    public class OldNotificationsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public OldNotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<NotificationDto> GetOldNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .OrderByDescending(n => n.NotificationDateTime)
+                .Take(count: 3)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+
+            return notifications.Select(Mapper.Map<Notification, NotificationDto>);
+        }
+    }
+}
--- JamCentral/JamCentral/Controllers/HomeController.cs
+++ JamCentral/JamCentral/Controllers/HomeController.cs
@@ -31,19 +31,5 @@ namespace JamCentral.Controllers
             };
             return View(viewModel);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }

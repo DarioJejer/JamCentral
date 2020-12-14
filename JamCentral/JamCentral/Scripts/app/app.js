f19bf16f47fbec61c32d6d86c15e39d8660ddb9b
Dario Jejer <dariojejer@gmail.com>
2020-12-14 18:52:42 -0300

    Extract first JavaScript code

---
+++ JamCentral/JamCentral/Scripts/app/app.js
@@ -0,0 +1,15 @@
+﻿function initGigs() {
+    $(".js-toggle-attendance").click(function (e) {
+        var button = $(e.target);
+        $.post("/api/attendences", { GigId: button.attr("data-gig-id") })
+            .done(function () {
+                button
+                    .removeClass("btn-light")
+                    .addClass("btn-info")
+                    .text("Going");
+            })
+            .fail(function () {
+                alert("Something failed!");
+            });
+    });
+}

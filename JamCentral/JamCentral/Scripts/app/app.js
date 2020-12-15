703c449fe1d6cb0d42d40b56d3b4cfd7cbd630be
Dario Jejer <dariojejer@gmail.com>
2020-12-15 19:55:01 -0300

    Create a Revealing Module Pattern file for toggle attendance

--- JamCentral/JamCentral/Scripts/app/app.js
+++ JamCentral/JamCentral/Scripts/app/app.js
@@ -1,15 +1,22 @@
-﻿function initGigs() {
-    $(".js-toggle-attendance").click(function (e) {
-        var button = $(e.target);
-        $.post("/api/attendences", { GigId: button.attr("data-gig-id") })
-            .done(function () {
-                button
-                    .removeClass("btn-light")
-                    .addClass("btn-info")
-                    .text("Going");
-            })
-            .fail(function () {
-                alert("Something failed!");
-            });
-    });
-}
+﻿var GigsController = function () {
+
+    var init = function () {
+        $(".js-toggle-attendance").click(function (e) {
+            var button = $(e.target);
+            $.post("/api/attendences", { GigId: button.attr("data-gig-id") })
+                .done(function () {
+                    button
+                        .removeClass("btn-light")
+                        .addClass("btn-info")
+                        .text("Going");
+                })
+                .fail(function () {
+                    alert("Something failed!");
+                });
+        });
+    };
+
+    return {
+        init: init
+    }
+}();

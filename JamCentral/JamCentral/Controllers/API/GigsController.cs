5d15d924e41eeb8c782646788623e73c88acf18c
Dario Jejer <dariojejer@gmail.com>
2020-12-22 19:37:41 -0300

    Refactor 'Uncancel' method on 'GigsController' API using repositories

--- JamCentral/JamCentral/Controllers/API/GigsController.cs
+++ JamCentral/JamCentral/Controllers/API/GigsController.cs
@@ -1,4 +1,5 @@
 ﻿using JamCentral.Models;
+using JamCentral.Persistence;
 using Microsoft.AspNet.Identity;
 using System.Data.Entity;
 using System.Linq;
@@ -10,29 +11,39 @@ namespace JamCentral.Controllers.API
     public class GigsController : ApiController
     {
         private ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public GigsController()
+        public GigsController(IUnitOfWork unitOfWork)
         {
             _context = new ApplicationDbContext();
+            _unitOfWork = unitOfWork;
         }
 
         [Authorize]
         [HttpPut]
         public IHttpActionResult Uncancel(int id)
         {
-            var userId = User.Identity.GetUserId();
+            //var userId = User.Identity.GetUserId();
 
-            var gig = _context.Gigs
-                .Include(g => g.Artist.Followers.Select(f => f.User))
-                .Include(g => g.Attendences.Select(a => a.Attendee))
-                .SingleOrDefault(g => g.Id == id && g.ArtistId == userId && g.IsCanceled);
+            var gig = _unitOfWork.Gigs.GetGigWithAttendanceAndFolllowers(id);
 
             if (gig == null)
                 return NotFound();
 
+            if (!gig.IsCanceled)
+                return BadRequest();
+
+            if (gig.ArtistId != User.Identity.GetUserId())
+                return Unauthorized();
+
+            //var gig = _context.Gigs
+            //    .Include(g => g.Artist.Followers.Select(f => f.User))
+            //    .Include(g => g.Attendences.Select(a => a.Attendee))
+            //    .SingleOrDefault(g => g.Id == id && g.ArtistId == userId && g.IsCanceled);
+
             gig.Uncancel();
 
-            _context.SaveChanges();
+            _unitOfWork.Complete();
 
             return Ok();
         }

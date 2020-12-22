133d3a89c9c2f450875957222ddd91a2395dbfd4
Dario Jejer <dariojejer@gmail.com>
2020-12-22 19:22:28 -0300

    Base iteration of 'GigsController' constructor

--- JamCentral/JamCentral.Tests/Controllers/API/GigsControllerTests.cs
+++ JamCentral/JamCentral.Tests/Controllers/API/GigsControllerTests.cs
@@ -13,7 +13,18 @@ namespace JamCentral.Tests.Controllers.API
     public class GigsControllerTests
     {
         public GigsControllerTests()
-        {       
+        {
+            var identity = new GenericIdentity("pepito@midominio.com");
+            identity.AddClaim(
+                new System.Security.Claims.Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "pepito@midominio.com"));
+            identity.AddClaim(
+                new System.Security.Claims.Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "1"));
+            var principal = new GenericPrincipal(identity, null);
+
+            var mockUoW = new Mock<IUnitOfWork>();
+            var controller = new GigsController(mockUoW.Object);
+
+            controller.ControllerContext.HttpContext.User = principal;
         }
 
         [TestMethod]

e551329f0587c515adcd81541ba2f0393389f659
Dario Jejer <dariojejer@gmail.com>
2020-12-22 19:15:00 -0300

    Create Unit Test Proyect

---
+++ JamCentral/JamCentral.Tests/Controllers/API/GigsControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using JamCentral.Controllers;
+using JamCentral.Persistence;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Mvc;
+
+namespace JamCentral.Tests.Controllers.API
+{
+    [TestClass]
+    public class GigsControllerTests
+    {
+        public GigsControllerTests()
+        {       
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ JamCentral/JamCentral.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JamCentral.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JamCentral.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c423578a-eae5-4167-835b-6f9473f8a624")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

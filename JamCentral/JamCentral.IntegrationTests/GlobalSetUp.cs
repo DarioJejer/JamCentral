1e425a44adddf77f3b724c9b3461b1620e8c0172
Dario Jejer <dariojejer@gmail.com>
2021-01-05 13:45:37 -0300

    Setup Seed method

--- JamCentral/JamCentral.IntegrationTests/GlobalSetUp.cs
+++ JamCentral/JamCentral.IntegrationTests/GlobalSetUp.cs
@@ -1,6 +1,7 @@
 ﻿using JamCentral.Models;
 using NUnit.Framework;
 using System.Data.Entity.Migrations;
+using System.Linq;
 
 namespace JamCentral.IntegrationTests
 {
@@ -10,9 +11,26 @@ namespace JamCentral.IntegrationTests
         [SetUp]
         public void SetUp()
         {
+            MigrateDbToLatest();
+
+            Seed();
+        }
+
+        private static void MigrateDbToLatest()
+        {
             var configuration = new JamCentral.Migrations.Configuration();
             var migrator = new DbMigrator(configuration);
             migrator.Update();
+        }
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+            if (context.Users.Any())
+                return;
+            context.Users.Add(new ApplicationUser { UserName = "user1", Name = "user1", Email = "-", PasswordHash = "-" });
+            context.Users.Add(new ApplicationUser { UserName = "user2", Name = "user2", Email = "-", PasswordHash = "-" });
+            context.SaveChanges();
         }
     }
 }

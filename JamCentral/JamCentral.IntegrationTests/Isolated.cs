4d88f3866a43e7f37130feffa70b219516f24c48
Dario Jejer <dariojejer@gmail.com>
2021-01-05 13:56:24 -0300

    Create attribute to revert changes on tests

---
+++ JamCentral/JamCentral.IntegrationTests/Isolated.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Transactions;
+
+namespace JamCentral.IntegrationTests
+{
+    public class Isolated : Attribute, ITestAction
+    {
+        private TransactionScope _transactionScope;
+
+        void ITestAction.BeforeTest(TestDetails testDetails)
+        {
+            _transactionScope = new TransactionScope();
+        }
+
+        void ITestAction.AfterTest(TestDetails testDetails)
+        {
+            _transactionScope.Dispose();
+        }
+        public ActionTargets Targets
+        {
+            get { return ActionTargets.Test; }
+        }
+    }
+}

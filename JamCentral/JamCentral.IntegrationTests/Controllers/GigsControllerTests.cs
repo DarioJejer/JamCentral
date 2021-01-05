2d90c37b8203d8b4c8aad7eeba9f1f05e4135afc
Dario Jejer <dariojejer@gmail.com>
2021-01-05 15:04:30 -0300

    Implement Integration Test for 'Update' action

--- JamCentral/JamCentral.IntegrationTests/Controllers/GigsControllerTests.cs
+++ JamCentral/JamCentral.IntegrationTests/Controllers/GigsControllerTests.cs
@@ -3,6 +3,7 @@ using JamCentral.Controllers;
 using JamCentral.IntegrationTests.Extensions;
 using JamCentral.Models;
 using JamCentral.Persistence;
+using JamCentral.ViewModels;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -43,6 +44,32 @@ namespace JamCentral.IntegrationTests.Controllers
             var result = _controller.Mine();
 
             (result.ViewData.Model as IEnumerable<Gig>).Should().HaveCount(1);
+        }
+
+        [Test, Isolated]
+        public void Update_CalledCorrectly_ModifyThegigOnDb()
+        {
+            var user = _context.Users.First();
+            _controller.MockCurrentUser(user.Id, user.UserName);
+            var genre = _context.Genres.Single(g => g.Id == 1);
+            var gig = new Gig(user.Id, "-", DateTime.Now.AddDays(1), genre.Id);
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            var viewModel = new GigFormViewModel
+            {
+                Id = gig.Id,
+                Location = "new",
+                Date = DateTime.Today.AddDays(2).ToString("d MMM yyyy"),
+                Time = "20:00",
+                GenreId = 2
+            };
+
+            var result = _controller.Update(viewModel);
+            
+            _context.Entry(gig).Reload();
+            gig.Date.Should().Be(DateTime.Today.AddDays(2).AddHours(20));
+            gig.Location.Should().Be("new");
+            gig.GenreId.Should().Be(2);
 
         }
     }

3835aecf6cd87ee2853297fda17514e802d2ce61
Dario Jejer <dariojejer@gmail.com>
2021-01-07 05:00:59 -0300

    Implement remaining tests for AttendancesController Integration Tests

--- JamCentral/JamCentral.IntegrationTests/Controllers/API/AttendancesControllerTests.cs
+++ JamCentral/JamCentral.IntegrationTests/Controllers/API/AttendancesControllerTests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using JamCentral.Dtos;
 using FluentAssertions;
 using System;
+using System.Web.Http.Results;
 
 namespace JamCentral.IntegrationTests.Controllers.API
 {
@@ -47,5 +48,45 @@ namespace JamCentral.IntegrationTests.Controllers.API
 
             _context.Attendences.Should().HaveCount(1);
         }
+
+        [Test, Isolated]
+        public void Attend_AttendanceExistInDb_ReturnBadRequest()
+        {
+            var gig = new Gig(_user.Id, "-", DateTime.Now.AddDays(1), _genre.Id);
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            var dto = new AttendenceDto { GigId = _context.Gigs.First().Id };
+            _controller.Attend(dto);
+
+            var result = _controller.Attend(dto);
+
+            result.Should().BeOfType<BadRequestErrorMessageResult>();
+        }
+
+        [Test, Isolated]
+        public void Unbook_CalledCorrectly_DeleteTheAttendance()
+        {
+            var gig = new Gig(_user.Id, "-", DateTime.Now.AddDays(1), _genre.Id);
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            var dto = new AttendenceDto { GigId = _context.Gigs.First().Id };
+            _controller.Attend(dto);
+
+            _controller.Unbook(dto);
+
+            _context.Attendences.Should().HaveCount(0);
+        }
+
+        [Test, Isolated]
+        public void Unbook_AttendanceNotInDb_ReturnBadRequest()
+        {
+            var dto = new AttendenceDto { GigId = 0 };                    
+
+            var result = _controller.Unbook(dto);
+
+            result.Should().BeOfType<BadRequestErrorMessageResult>();
+        }
+
+
     }
 }

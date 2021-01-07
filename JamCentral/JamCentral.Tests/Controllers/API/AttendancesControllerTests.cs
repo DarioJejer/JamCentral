ba2d677c7bcb400e6d17f368b66baba03b877e19
Dario Jejer <dariojejer@gmail.com>
2021-01-07 03:14:38 -0300

    Create and SetUp 'AttendancesControllerTests'

---
+++ JamCentral/JamCentral.Tests/Controllers/API/AttendancesControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using JamCentral.Controllers.API;
+using JamCentral.Persistence;
+using JamCentral.Repositories;
+using JamCentral.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace JamCentral.Tests.Controllers.API
+{
+    [TestClass]
+    public class AttendancesControllerTests
+    {
+        private Mock<IAttendencesRepository> _mockRepository;
+        private AttendencesController _controller;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockRepository = new Mock<IAttendencesRepository>();
+             var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(u => u.Attendences).Returns(_mockRepository.Object);
+            _controller = new AttendencesController(mockUnitOfWork.Object);
+            _controller.MockCurrentUser("1", "pepito@midominio.com");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
